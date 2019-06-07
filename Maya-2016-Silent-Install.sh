eabda3650948fbb1cecb6557f3199f0551cc16bc
Gary Lee <gary@gazlee.co.uk>
2019-06-07 15:41:57 +0100

    Added Maya 2016

---
+++ Maya-2016-Silent-Install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+if [[ -e /tmp/Install\ Maya\ 2016.app/Contents/MacOS/setup ]]; then
+    /tmp/Install\ Maya\ 2016.app/Contents/MacOS/setup --noui --serial_number=560-xxxxxxxx --product_key=793H1 --license_type=kNetwork --server_name=your-server.net
+
+    if [[ ! -e /Library/Application\ Support/Autodesk/CLM/LGS/657H1_2016.0.0.F ]]; then
+        mkdir -p /Library/Application\ Support/Autodesk/CLM/LGS/657H1_2016.0.0.F
+    fi
+
+    cat<<EOF>/Library/Application\ Support/Autodesk/CLM/LGS/657H1_2016.0.0.F/LGS.data
+_NETWORK
+EOF
+
+    cat<<EOF>/Library/Application\ Support/Autodesk/CLM/LGS/657H1_2016.0.0.F/LICPATH.lic
+SERVER your-server.net 000000000000
+USE_SERVER
+EOF
+
+    cat<<EOF>/Library/Application\ Support/Autodesk/CLM/LGS/657H1_2016.0.0.F/nw.cfg
+done
+EOF
+
+else
+    echo "ERROR: Maya installer not found."
+    exit 1
+fi
+
+rm -rf /tmp/Install\ Maya\ 2016.app
+
+exit 0
